48c27033fe17a030d3b6a5ecec3f9091e2e74940
Louis Andrew <louisandrew3@gmail.com>
2021-11-16 11:19:05 +0100

    feat: render box value inside grid item

--- src/components/TrackItem/Grid/GridBackround.jsx
+++ src/components/TrackItem/Grid/GridBackround.jsx
@@ -6,6 +6,12 @@ const GridBackround = () => {
   const GRID_ROWS = 8;
   const GRID_COLS = 16;
 
+  const activeBox = ['1;2', '7;12'];
+  const activeBoxValues = {
+    '1;2': 3,
+    '7;12': 1,
+  };
+
   /**
    * @param {number} length
    */
@@ -13,26 +19,41 @@ const GridBackround = () => {
 
   return (
     <Stack
-      container
       spacing={0.25}
     >
       {createArray(GRID_ROWS).map((_, index) => (
         <Stack direction="row" spacing={0.25} key={`row-${index}`}>
           {
-            createArray(GRID_COLS).map((s, columnIndex) => (
-              <Box
-                sx={{
-                  width: 30,
-                  height: 30,
-                  backgroundColor: 'primary.dark',
-                  '&:hover': {
-                    backgroundColor: 'primary.main',
-                    opacity: [0.9, 0.8, 0.7],
-                  },
-                }}
-                key={`col-${columnIndex}`}
-              />
-            ))
+            createArray(GRID_COLS).map((s, columnIndex) => {
+              const id = `${index};${columnIndex}`;
+              const idValue = activeBoxValues[id];
+              return (
+                <Box
+                  sx={{
+                    width: 30,
+                    height: 30,
+                    backgroundColor: 'primary.dark',
+                    '&:hover': {
+                      backgroundColor: 'primary.main',
+                      opacity: [0.9, 0.8, 0.7],
+                    },
+                  }}
+                  key={`col-${columnIndex}`}
+                >
+                  {activeBox.includes(id) && idValue
+                    ? (
+                      <Box sx={{
+                        width: idValue * 30 + (idValue - 1) * 2,
+                        height: 30,
+                        backgroundColor: '#faa',
+                        position: 'relative',
+                        zIndex: 100,
+                      }}
+                      />
+                    ) : null}
+                </Box>
+              );
+            })
           }
         </Stack>
       ))}
