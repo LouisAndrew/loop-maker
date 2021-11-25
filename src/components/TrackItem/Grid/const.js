a0e483def955bbc59fbd003eb3645535acf551eb
Louis Andrew <louisandrew3@gmail.com>
2021-11-25 11:49:50 +0100

    feat(tone): add functionality to play grid item track

--- src/components/TrackItem/Grid/const.js
+++ src/components/TrackItem/Grid/const.js
@@ -1,8 +1,16 @@
-const NOTES = ['C', 'B', 'A', 'G', 'F', 'E', 'D', 'C'];
+const BASE_NOTES = ['B', 'A', 'G', 'F', 'E', 'D', 'C'];
+const NOTES = ['C', ...BASE_NOTES, ...BASE_NOTES];
 const DELIMITER = '__';
-const GRID_ROWS = 8;
-const GRID_COLS = 32;
+const GRID_ROWS = 15;
+const GRID_COLS = 48;
+const NOTATIONS = ['1n', '2n', '4n', '8n'];
+const NOTATION_VALUES = {
+  '1n': 8,
+  '2n': 4,
+  '4n': 2,
+  '8n': 1,
+};
 
 export {
-  NOTES, DELIMITER, GRID_COLS, GRID_ROWS,
+  NOTES, DELIMITER, GRID_COLS, GRID_ROWS, NOTATIONS, NOTATION_VALUES,
 };
--- src/components/TrackItem/TrackGrid.jsx
+++ src/components/TrackItem/TrackGrid.jsx
@@ -1,8 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import * as Tone from 'tone';
+import { DELIMITER, NOTATION_VALUES, NOTES } from './Grid/const';
+
 import GridItem from './Grid/GridItem';
 
-const TrackGrid = () => (
-  <GridItem trackColor="yellow" />
-);
+const TrackGrid = () => {
+  /**
+   * @type {[Tone.Synth]}
+   */
+  const [player, setPlayer] = useState(null);
+
+  useEffect(() => {
+    setPlayer(new Tone.Synth().toDestination());
+  }, []);
+
+  /**
+   * Divide a number to its floored value (whole number division) and its rest.
+   * @param {number} num Number to be divided.
+   * @param {number} divider Divider of the number
+   * @returns {[number, number]} result of the division and the rest of the division
+   */
+  const divide = (num, divider) => [Math.floor(num / divider), num % divider];
+
+  /**
+   * Function to create a Tone.js compliant time object from a duration.
+   * @param {number} duration Duration of the time in grid units.
+   * @returns {Object} Tone.js compliant time object.
+   */
+  const createTimeObject = (duration) => {
+    // note: Assuming that every grid unit -> 8n or 1/8 note
+    const [nValue, nRest] = divide(duration, NOTATION_VALUES['1n']);
+    const [halfNValue, halfNRest] = divide(nRest, NOTATION_VALUES['2n']);
+    const [quarterNValue, quarterNRest] = divide(
+      halfNRest,
+      NOTATION_VALUES['4n'],
+    );
+    const [eighthNValue] = divide(quarterNRest, NOTATION_VALUES['8n']);
+
+    return {
+      '1n': nValue,
+      '2n': halfNValue,
+      '4n': quarterNValue,
+      '8n': eighthNValue,
+    };
+  };
+
+  /**
+   * Function to play a grid item.
+   * @param {string[]} items
+   */
+  const play = async (items) => {
+    const times = items.map((item) => {
+      const [row, col, duration] = item.split(DELIMITER);
+      return {
+        row,
+        duration: createTimeObject(duration),
+        start: player.toSeconds(createTimeObject(col)),
+      };
+    });
+
+    times.forEach(({ duration, start, row }) => {
+      const getNote = () => {
+        const OCTAVES = [5, 4, 3];
+        const note = NOTES[row];
+        const octave = OCTAVES[Math.floor(row / 7)];
+        return `${note}${note === 'C' ? octave + 1 : octave}`;
+      };
+
+      player.triggerAttackRelease(getNote(), duration, `+${start}`);
+    });
+
+    console.log({ last: times[times.length - 1].start });
+
+    await Tone.start();
+  };
+
+  return <GridItem trackColor="yellow" onPlay={play} />;
+};
 
 export default TrackGrid;
