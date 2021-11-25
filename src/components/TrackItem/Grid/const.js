5f3f9486a716464c8ea2c16d707a1dcd206731fc
Louis Andrew <louisandrew3@gmail.com>
2021-11-25 15:06:07 +0100

    fix(track-grid): fix race condition on parallel tone and add c# note

--- src/components/TrackItem/Grid/const.js
+++ src/components/TrackItem/Grid/const.js
@@ -1,7 +1,7 @@
-const BASE_NOTES = ['B', 'A', 'G', 'F', 'E', 'D', 'C'];
+const BASE_NOTES = ['B', 'A', 'G', 'F', 'E', 'D', 'C#', 'C'];
 const NOTES = ['C', ...BASE_NOTES, ...BASE_NOTES];
 const DELIMITER = '__';
-const GRID_ROWS = 15;
+const GRID_ROWS = 17;
 const GRID_COLS = 48;
 const NOTATIONS = ['1n', '2n', '4n', '8n'];
 const NOTATION_VALUES = {
