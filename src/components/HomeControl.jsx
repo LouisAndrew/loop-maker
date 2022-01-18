96ed2f04d98ce1a5432f3067804ed19a37f62e09
s0573804 <farida.elbassiouney@student.htw-berlin.de>
2022-01-18 23:17:49 +0100

    updated HomeControl

--- src/components/HomeControl.jsx
+++ src/components/HomeControl.jsx
@@ -1,7 +1,66 @@
 import React from 'react';
+import {
+  Stack, Box, InputLabel, Slider, FormControl, Select, MenuItem,
+} from '@mui/material';
 
-const HomeControl = () => (
-  <div />
-);
+import { useTracks } from '../hooks/useTracks';
+
+const HomeControl = () => {
+  const {
+    tempo,
+    gridLength, getSetter,
+  } = useTracks();
+
+  const { setTempo, setGridLength } = getSetter(1);
+
+  return (
+    <div>
+      <Stack direction="row" alignItems="flex-end" spacing={1}>
+        {tempo && (
+        <Box sx={{ width: 120 }}>
+          <InputLabel id="tempo">
+            Tempo:
+            {' '}
+            {tempo}
+          </InputLabel>
+          <Slider
+            size="small"
+            value={tempo}
+            id="tempo"
+            max={180}
+            min={80}
+            onChange={(_, newValue) => setTempo(newValue)}
+          />
+        </Box>
+        )}
+        {gridLength && (
+        <Box sx={{ width: 80 }}>
+          <FormControl size="small" sx={{ minWidth: 80 }}>
+            <InputLabel id="grid-size">
+              Grid length:
+              {gridLength}
+            </InputLabel>
+            <Select
+              labelId="grid-size"
+              id="grid-size-input"
+              value={gridLength}
+              label="Grid length"
+              autoWidth
+              variant="filled"
+              onChange={(e) => setGridLength(parseInt(e.target.value, 10))}
+            >
+              {[32, 36, 40, 44, 48, 52].map((len) => (
+                <MenuItem key={len} value={len}>
+                  {len}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>
+        )}
+      </Stack>
+    </div>
+  );
+};
 
 export default HomeControl;
